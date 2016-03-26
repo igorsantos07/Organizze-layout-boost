62bc24574d2a18ce8b346cf490c328b5663660d7
Igor Santos <igorsantos07@gmail.com>
2016-03-25 21:03:33 -0300

    Using dollars when there's a dollar sign in the account name

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(function() {
   $(document).on('dblclick', '#transactions-table tr.CreditCard', function() {
-    $tr = $(this);
+    $tr = $(this)
     if ($tr.data('selected')) {
       $tr
         .css({ backgroundColor: 'inherit' })
@@ -11,4 +11,48 @@ $(function() {
         .data('selected', true)
     }
   })
-});
+
+  function intoDollars($element) {
+    $element = (typeof $element == 'object')? $element : $(this)
+    $element
+      .text($element.text().replace('R$', '$'))
+      .addClass('dollar')
+  }
+
+  new MutationObserver(function(mutations, observer) {
+    console.count('Mutated')
+
+    //fixing dollar sign on dashboard accounts list
+    var $accounts = $('.accounts-list li:not(.currencied)')
+    if ($accounts.length > 0) {
+      $accounts.each(function() {
+        console.count('Looking for a Dollar account')
+        var $account = $(this)
+        var account = $account.find('.name').text()
+        var value = $account.find('.value')
+
+        if (account.indexOf('$') >= 0) {
+          console.log('Conta '+account+' agora aparece em dólares.')
+          intoDollars($account.find('.value'))
+          
+        }
+        
+        $account.addClass('currencied') //this class avoids multiple runs when other mutations happen
+      })
+    }
+
+    //fixing dollar sign on transactions list
+    var $table = $('table.transactions-table')
+    if ($table.length > 0) {
+      if ($('.filter-account .top-level').text().indexOf('$') >= 0) {
+        $table.find('td.amount:not(.dollar), .results-detail strong:not(.dollar)').each(intoDollars)
+        $('.details-transactions td big:not(.dollar), .final-balance strong:not(.dollar)').each(intoDollars)
+      }
+    }
+  }).observe(document, {
+    childList: true,
+    subtree: true
+  })
+
+  setTimeout(function() { $('body').addClass('dollarObserver') }, 100) //forcing a mutation on page load
+})
