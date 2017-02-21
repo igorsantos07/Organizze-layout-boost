a996c3cd8fa7f06c2d3716ec159c54e60724fa70
Igor Santos <igorsantos07@gmail.com>
2017-02-21 12:11:04 -0300

    Melhorando o patch no menu e corrigindo o domínio
    - agora o patch do menu é feito por mutation observer, o que significa que as novas entradas são permanentes através das páginas!
    - contas e cartões também foram adicionados de volta no menu de configurações, já que eles foram movidos para a parte de 'lançamentos'
    - o histórico de atividade foi para o menu de ferramentas
    - a remoção de decimais vazios do bloco de metas voltou a funcionar

--- main.js
+++ main.js
@@ -18,87 +18,115 @@ $(function() {
   })
 
 /* ******************************************************************************* */
-/* ********************  ADDING ACTIVITY HISTORY TO SETTINGS  ******************** */
-/* ******************************************************************************* */
-
-var $settings = $('.configurations .drop-down-box ul')
-var userId = $settings.find('li').first().find('a').attr('href')
-userId = userId.substr(1)
-userId = userId.substr(0, userId.indexOf('/'))
-
-$settings.append($(
-  "<li>"+
-    "<a href='/"+userId+"/configuracoes/atividades'>Histórico de Atividades</a>"+
-  "</li>"
-))
-
-/* ******************************************************************************* */
 /* ****************  CLEARS UP USELESS STRINGS IN BUDGETS WIDGET  **************** */
 /* ******************************************************************************* */
 
-//shortens up the main budget title
-$firstBudget = $('body.dashboard .widget_box.budgets .box-content ul li:first-child .info big')
-if ($firstBudget.text().indexOf('Meta geral de despesa') != -1) {
-  $firstBudget.text('Geral - despesas')
-}
+	//shortens up the main budget title
+	$firstBudget = $('body.dashboard .widget_box.budgets .box-content ul li:first-child .info big')
+	if ($firstBudget.text().indexOf('Meta geral de despesa') != -1) {
+		$firstBudget.text('Geral - despesas')
+	}
 
-//removes empty cents
-$('body.dashboard .widget_box.budgets .box-content ul li').each(function() {
-  $(this).find('.info p strong').each(function() {
-    if (this.innerText.indexOf(',00') != -1) {
-      $(this).text(this.innerText.substr(0, this.innerText.length - 3))
-    }
-  })
-})
+	//removes empty cents
+	$('#user_account_widget_budgets .box-content ul li').each(function() {
+		$(this).find('.info p strong').each(function() {
+		  if (this.innerText.indexOf(',00') != -1) {
+		    $(this).text(this.innerText.substr(0, this.innerText.length - 3))
+		  }
+		})
+	})
+
+/* ******************************************************************************* */
+/* ****************************  ADDING MENU ENTRIES  **************************** */
+/* ******************************************************************************* */
+
+	var userId = $('.settings .drop-down-box ul li:first a').attr('href')
+	userId = userId.substr(1)
+	userId = userId.substr(0, userId.indexOf('/'))
+	console.log(`User ID: ${userId}`)
+
+	//to be used in the mutation observer below
+	function addMenuEntries($header) {
+		$header.find('.tools .drop-down-box ul')
+			.append($(
+				"<li>"+
+					"<a href='/"+userId+"/configuracoes/atividades'>Histórico de Atividades</a>"+
+				"</li>"
+			))
+
+		$header.find('.settings .drop-down-box ul')
+			.find('a[href*=categorias]')
+			.after($(
+				"<li>"+
+					"<a href='/"+userId+"/a/contas'>Contas e carteiras</a>"+
+				"</li>"+
+				"<li>"+
+					"<a href='/"+userId+"/cartao-de-credito'>Cartões de Crédito</a>"+
+				"</li>"
+			))
+	}
 
 /* ******************************************************************************* */
 /* *******************  DOLLAR SIGN IN INTERNATIONAL ACCOUNTS  ******************* */
 /* ******************************************************************************* */
 
-  function intoDollars($element) {
-    $element = (typeof $element == 'object')? $element : $(this)
-    $element
-      .text($element.text().replace('R$', '$'))
-      .addClass('dollar')
-  }
+	//to be used in the mutation observer below
+	function intoDollars($element) {
+	$element = (typeof $element == 'object')? $element : $(this)
+	$element
+		.text($element.text().replace('R$', '$'))
+		.addClass('dollar')
+	}
 
-  new MutationObserver(function(mutations, observer) {
-    console.count('Mutated')
+/* ******************************************************************************* */
+/* *******************  DOLLAR SIGN IN INTERNATIONAL ACCOUNTS  ******************* */
+/* ******************************************************************************* */
 
-    //fixing dollar sign on dashboard accounts list
-    var $accounts = $('.accounts-list li:not(.currencied)')
-    if ($accounts.length > 0) {
-      $accounts.each(function() {
-        console.count('Looking for a Dollar account')
-        var $account = $(this)
-        var account = $account.find('.name').text()
-        var value = $account.find('.value')
+	new MutationObserver(function(mutations, observer) {
+		//console.count('Mutated')
 
-        if (account.indexOf('$') >= 0) {
-          console.log('Conta '+account+' agora aparece em dólares.')
-          intoDollars($account.find('.value'))
-          
-        }
-        
-        //this class avoids multiple runs when other mutations happen
-        $account.addClass('currencied')
-      })
-    }
+		//fixing dollar sign on dashboard accounts list
+		var $accounts = $('.accounts-list li:not(.currencied)')
+		if ($accounts.length > 0) {
+		  $accounts.each(function() {
+		    //console.count('Looking for a Dollar account')
+		    var $account = $(this)
+		    var account = $account.find('.name').text()
+		    var value = $account.find('.value')
 
-    //fixing dollar sign on transactions list
-    var $table = $('table.transactions-table')
-    if ($table.length > 0) {
-      if ($('.filter-account .top-level').text().indexOf('$') >= 0) {
-        $table.find('td.amount:not(.dollar)').each(intoDollars)
-		//FIXME: these two are dangerous, as they break data updates from Angular
-		//$table.find('.results-detail strong:not(.dollar)').each(intoDollars)
-        //$('.details-transactions td big:not(.dollar), .final-balance strong:not(.dollar)').each(intoDollars)
-      }
-    }
-  }).observe(document, {
-    childList: true,
-    subtree: true
-  })
+		    if (account.indexOf('$') >= 0) {
+		      console.log('Conta '+account+' agora aparece em dólares.')
+		      intoDollars($account.find('.value'))
+		      
+		    }
+		    
+		    //this class avoids multiple runs when other mutations happen
+		    $account.addClass('currencied')
+		  })
+		}
 
-  setTimeout(function() { $('body').addClass('dollarObserver') }, 100) //forcing a mutation on page load
+		//fixing dollar sign on transactions list
+		var $table = $('table.transactions-table')
+		if ($table.length > 0) {
+		  if ($('.filter-account .top-level').text().indexOf('$') >= 0) {
+		    $table.find('td.amount:not(.dollar)').each(intoDollars)
+				//FIXME: these two are dangerous, as they break data updates from Angular
+				//$table.find('.results-detail strong:not(.dollar)').each(intoDollars)
+		    //$('.details-transactions td big:not(.dollar), .final-balance strong:not(.dollar)').each(intoDollars)
+		  }
+		}
+
+		//adding menu entries
+		$header = $('body > header')
+		if (!$header.hasClass('expanded')) { //a guard so we don't add stuff repeatedly
+			addMenuEntries($header)
+			$header.addClass('expanded')
+		}
+	}).observe(document, {
+		childList: true,
+		subtree: true
+	})
+
+	//forcing a mutation on page load
+  setTimeout(function() { $('body').addClass('dollarObserver') }, 100)
 })
